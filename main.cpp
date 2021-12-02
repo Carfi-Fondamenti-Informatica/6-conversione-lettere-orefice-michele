fc19533198a3733308d39ec8ffe754122bf5f1f4
orefice-michele <91534750+orefice-michele@users.noreply.github.com>
2021-12-02 01:16:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
-
-int main(){
-  return 0;
+using namespace std;
+bool ris=0;
+void riconoscimento(char &a){
+  if(a<91&&a>64) {
+    ris=true;
+    a=a+32;
+  }
+  else if (a<123&&a>96){
+    ris=true;
+    a=a-32;
+  }
+  else {
+    ris=false;
+  }
+  
+ int main(){
+  char input=0;
+   cin>> input;
+   riconoscimento(input);
+   if(ris==true){
+     cout<<"true"<<endl;
+     cout<<input<<endl;
+   }
+   else{
+     cout<<"false"<<endl;
+   }
+     
+   return 0;
 }
